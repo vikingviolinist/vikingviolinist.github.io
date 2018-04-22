92fa887bbfc533ed736dbe1da3484a042d627516
vikingviolinist <misopetrik@gmail.com>
2018-04-22 19:46:28 +0200

    Add violin welcome page with styles and script

--- js/agency.js
+++ js/agency.js
@@ -57,4 +57,88 @@ $(window).on('scroll', function(){
 
 })(jQuery);
 
+// Add scroll to scroll button
+  $(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 670, 'linear');
+    });
+  });
+  
+ $(document).scroll(function() {
+  const y = $(this).scrollTop();
+  const box = document.getElementsByClassName('box')[0];
+  if (y > 900) {
+    $(box).fadeIn(1300);
+  } else {
+    $(box).fadeOut();
+  }
+});
+
+  // Add scroll to violin buttons
+  const buttons = document.getElementsByClassName('buttons');
+  for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    $('html,body').animate({
+        scrollTop: $('#section-two').offset().top}, 1500, 'linear');
+  });
+  };
+
+  // Add event listeners to buttons
+  const buttonNorwegian = document.getElementById('buttonNorwegian');
+  buttonNorwegian.addEventListener('mouseover', function() {
+    buttonNorwegian.style.fillOpacity = 0.7;
+  });
+  buttonNorwegian.addEventListener('mouseout', function() {
+    buttonNorwegian.style.fillOpacity = 0;
+  });
+  buttonNorwegian.addEventListener('click', function() {
+    const sections = document.getElementsByTagName('h1');
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].textContent = 'Dette er en tekst på norsk.';
+    }
+  });
+
+  const buttonEnglish = document.getElementById('buttonEnglish');
+  buttonEnglish.addEventListener('mouseover', function() {
+    buttonEnglish.style.fillOpacity = 0.7;
+  });
+  buttonEnglish.addEventListener('mouseout', function() {
+    buttonEnglish.style.fillOpacity = 0;
+  });
+  buttonEnglish.addEventListener('click', function() {
+    const sections = document.getElementsByTagName('h1');
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].textContent = 'This is a text in English.';
+    }
+  });
+
+  const buttonGerman = document.getElementById('buttonGerman');
+  buttonGerman.addEventListener('mouseover', function() {
+    buttonGerman.style.fillOpacity = 0.7;
+  });
+  buttonGerman.addEventListener('mouseout', function() {
+    buttonGerman.style.fillOpacity = 0;
+  });
+  buttonGerman.addEventListener('click', function() {
+    const sections = document.getElementsByTagName('h1');
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].textContent = 'Dies ist ein Text auf Deutsch.';
+    }
+  });
+
+  const buttonSlovak = document.getElementById('buttonSlovak');
+  buttonSlovak.addEventListener('mouseover', function() {
+    buttonSlovak.style.fillOpacity = 0.7;
+  });
+  buttonSlovak.addEventListener('mouseout', function() {
+    buttonSlovak.style.fillOpacity = 0;
+  });
+  buttonSlovak.addEventListener('click', function() {
+    const sections = document.getElementsByTagName('h1');
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].textContent = 'Toto je text v slovenčine.';
+    }
+  });
+
 
